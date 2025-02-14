d0b51e9495312a102e0983be956a35c97cf27d58
Abubakr Wamiq <121969132+abubakr521@users.noreply.github.com>
2025-02-14 19:45:13 +0500

    Add files via upload

---
+++ The Swords Owner/chal1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("What is the one thing a true friend has that he should protect, with his heart and soul?")
+user_input = input("> ").strip().lower()
+
+if user_input == "dream":
+    print("AxP25{m45t3r_0f_th3_5w0rd}")
+else:
+    print("AxP25{TrY_H4rd3r_N3v3r_G1v3_Up}")
